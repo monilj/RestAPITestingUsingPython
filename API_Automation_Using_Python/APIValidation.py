a58987999403b9c7c3ae5a6807dcde64eff4a81b
Monil Joshi <monil.joshi@qualitia.local>
2020-12-11 20:43:24 +0530

    API Autimation using Python | API Validation Basics | Validating response status codes and headers using response object | Traverse through response json and assert desired data

--- API_Automation_Using_Python/APIValidation.py
+++ API_Automation_Using_Python/APIValidation.py
@@ -9,3 +9,17 @@ response = requests.get(' http://216.10.245.166/Library/GetBook.php',
 # dict_response = json.loads(response.text)
 json_response = response.json()
 print(json_response)
+print(response.status_code)
+assert response.status_code == 200
+print(response.headers)
+assert response.headers['content-type'] == 'application/json;charset=UTF-8'
+
+# Retrieve the book details with ISBN abc123xyz1
+print("=====  Retrieve the book details with ISBN abc123xyz1 =====")
+for det in json_response:
+    if det['isbn'] == 'abc123xyz1':
+        print(det['book_name'])
+        print(det['aisle'])
+    if det['isbn'] == 'PSRS':
+        print(det['book_name'])
+        print(det['aisle'])
