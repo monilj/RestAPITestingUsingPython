cf3758b2df4799202e31849246ce912e452d04ae
Monil Joshi <monil.joshi@qualitia.local>
2020-12-21 10:40:07 +0530

    API Automation using Python | API Validation Basics | Added one more POST api validation which DELETEs created resources

--- API_Automation_Using_Python/APIValidation_POSTRequest.py
+++ API_Automation_Using_Python/APIValidation_POSTRequest.py
@@ -10,3 +10,12 @@ addBook_response = requests.post('http://216.10.245.166/Library/AddBook.php', js
 print(addBook_response.json())
 resp = addBook_response.json()
 bookId = resp['Id']
+
+resp_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
+    "ID": bookId
+}, headers={"content-type": "application/json"}, )
+
+assert resp_deleteBook.status_code == 200
+resp_json = resp_deleteBook.json()
+# add assert statements
+
