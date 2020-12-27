b3cb79539c9301383504166a46998bca9b73c0b9
Monil Joshi <monil.joshi@qualitia.local>
2020-12-27 23:56:54 +0530

    API Automation using Python | Adding a class for Resources for Add and Delete API | Import and Use all these in POST request | Optimize POST request by add a variable called url and calling it in request

--- API_Automation_Using_Python/APIValidation_POSTRequest.py
+++ API_Automation_Using_Python/APIValidation_POSTRequest.py
@@ -3,18 +3,23 @@ import configparser
 
 from API_Automation_Using_Python.PayLoad import addBookPayLoad
 from util import configurations
+from util.resources import ApiResources
 
-addBook_response = requests.post(configurations.getConfig()['API']['endpoint'] + '/Library/AddBook.php',
+url = configurations.getConfig()['API']['endpoint'] + ApiResources.addBook
+headers = {"content-type": "application/json"}
+addBook_response = requests.post(url,
                                  json=addBookPayLoad()
-                                 , headers={"content-type": "application/json"}, )
+                                 , headers=headers, )
 
 print(addBook_response.json())
 resp = addBook_response.json()
 bookId = resp['Id']
 
-resp_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
+deleteUrl = configurations.getConfig()['API']['endpoint'] + ApiResources.deleteBook
+deleteHeaders = {"content-type": "application/json"}
+resp_deleteBook = requests.post(deleteUrl, json={
     "ID": bookId
-}, headers={"content-type": "application/json"}, )
+}, headers=deleteHeaders, )
 
 assert resp_deleteBook.status_code == 200
 resp_json = resp_deleteBook.json()
---
+++ util/resources.py
@@ -0,0 +1,3 @@
+class ApiResources:
+    addBook = "/Library/AddBook.php"
+    deleteBook = "/Library/DeleteBook.php"
