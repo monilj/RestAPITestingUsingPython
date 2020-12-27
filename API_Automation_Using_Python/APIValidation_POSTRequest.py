4fd3a007090b8f0317083bd70526950c6b3c5f30
Monil Joshi <monil.joshi@qualitia.local>
2020-12-27 22:54:43 +0530

    API Automation using Python | API Validations Basics | Added seperated file for json Payload

--- API_Automation_Using_Python/APIValidation_POSTRequest.py
+++ API_Automation_Using_Python/APIValidation_POSTRequest.py
@@ -1,11 +1,9 @@
 import requests
 
-addBook_response = requests.post('http://216.10.245.166/Library/AddBook.php', json={
-    "name": "Learn Appium Automation with Java",
-    "isbn": "bwd",
-    "aisle": "327",
-    "author": "John foe"
-}, headers={"content-type": "application/json"}, )
+from API_Automation_Using_Python.PayLoad import addBookPayLoad
+
+addBook_response = requests.post('http://216.10.245.166/Library/AddBook.php', json=addBookPayLoad()
+                                 , headers={"content-type": "application/json"}, )
 
 print(addBook_response.json())
 resp = addBook_response.json()
@@ -18,4 +16,3 @@ resp_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', 
 assert resp_deleteBook.status_code == 200
 resp_json = resp_deleteBook.json()
 # add assert statements
-
---
+++ API_Automation_Using_Python/PayLoad.py
@@ -0,0 +1,8 @@
+def addBookPayLoad():
+    body ={
+        "name": "Learn Appium Automation with Java",
+        "isbn": "bwd",
+        "aisle": "327",
+        "author": "John foe"
+    }
+    return body
