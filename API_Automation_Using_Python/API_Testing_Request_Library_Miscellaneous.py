f6112ad330202c6c2c71ff5285833a389e078b91
monilj <moniljoshi786@gmail.com>
2021-04-24 23:47:15 +0530

    API Automation using Pyhton | Timeout and Redirect Attribute

--- API_Automation_Using_Python/API_Testing_Request_Library_Miscellaneous.py
+++ API_Automation_Using_Python/API_Testing_Request_Library_Miscellaneous.py
@@ -1,13 +1,14 @@
 import requests
 
 cookie = {'visit-month': 'Febuary'}
-# requests.get("https://rahulshettyacademy.com/", cookies=cookie)
-
+rep = requests.get("https://rahulshettyacademy.com/",allow_redirects= False, cookies=cookie, timeout=4)
+print(rep.history)
+print(rep.status_code)
 
 se = requests.session()
 se.cookies.update(cookie)
-res= se.get("https://httpbin.org/cookies")
+# res = se.get("https://httpbin.org/cookies")
 # res= requests.get("https://httpbin.org/cookies",cookies=cookie)
-res = se.get("https://httpbin.org/cookies", cookies={'visit-year': '2020'})
-
-print(res.text)
+# response = se.get("https://httpbin.org/cookies", cookies={'visit-year': '2020'})
+# print(response.history)
+# print(res.text)
