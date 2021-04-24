5c49ed59b95d943e0fbf2b64f7b2fb5cbaeddb5f
monilj <moniljoshi786@gmail.com>
2021-04-24 12:29:55 +0530

    API automation using Python | Add session stream for Auth

--- API_Automation_Using_Python/Simple_Authentication.py
+++ API_Automation_Using_Python/Simple_Authentication.py
@@ -1,8 +1,17 @@
 import requests
 
-url = "https://api.github.com/user"
-git_response = requests.get(url, auth=('monilj', 'password'))
-print(git_response.status_code)
+password = 'mmmmmm'
 
+ses = requests.session()
+ses.auth = auth = ('monilj', password)
+
+url = "https://api.github.com/user"
+# git_response = requests.get(url, auth=('monilj', password))
+git_response = ses.get(url)
+# print(git_response.status_code)
+
+url_2 = "https://api.github.com/user/get/user/repo"
+# git_response3 = requests.get(url_2, auth=('monilj', password))
+git_response3 = ses.get(url_2)
 # for SSL
-git_response = requests.get(url, verify=False, auth=('monilj', 'password'))
+# git_response = requests.get(url, verify=False, auth=('monilj', 'password'))
