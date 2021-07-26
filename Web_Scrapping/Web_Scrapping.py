cf883a9bbe6c6258131d09e8eab2d156e5077185
moniljoshi <monil.joshi@qualitia.com>
2021-07-26 22:53:00 +0530

    Extracting Visible content

--- Web_Scrapping/Web_Scrapping.py
+++ Web_Scrapping/Web_Scrapping.py
@@ -10,3 +10,10 @@ rows = movieTable.findAll('tr')
 for row in rows:
     rowData = row.findAll('td')
     print(rowData[1].find('a').text)
+    subURL = rowData[1].a['href']
+    subData = requests.get("https://www.imdb.com" + subURL)
+    childSoup = BeautifulSoup(subData.content, 'html.parser')
+    if childSoup.find('div',{'class':'see-more inline canwrap'}):
+       genre= childSoup.find('div',{'class':'see-more inline canwrap'})
+       if genre.a.text==" Documentry":
+           print(rowData[1].a.text)
